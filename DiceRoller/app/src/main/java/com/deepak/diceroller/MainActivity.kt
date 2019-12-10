02aff157625055ae452ea5460d7f47a72da8ca09
deepak kumar <deepak24121994@gmail.com>
2019-12-10 11:26:16 +0530

    use of lateInit
    so that find view by id not need to be searched every time roll() call

--- DiceRoller/app/src/main/java/com/deepak/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/deepak/diceroller/MainActivity.kt
@@ -7,11 +7,12 @@ import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener {
             roll()
         }
@@ -20,9 +21,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun roll() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+
         val randomInt = Random.nextInt(6) + 1
-        var drawableResourc = when (randomInt) {
+        val drawableResourc = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
