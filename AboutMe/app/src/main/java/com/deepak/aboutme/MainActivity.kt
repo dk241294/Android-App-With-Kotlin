a2505e520c094b7af3f0490bf00594332f77396e
deepak kumar <deepak24121994@gmail.com>
2019-12-11 13:37:46 +0530

    data binding
    layout,data,variable

--- AboutMe/app/src/main/java/com/deepak/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/deepak/aboutme/MainActivity.kt
@@ -10,27 +10,41 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.isGone
+import androidx.databinding.DataBindingUtil
+import com.deepak.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val name: MyName = MyName("Deepak Kumar")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val doneButton:Button =findViewById(R.id.done_button)
-        doneButton.setOnClickListener{
-           addNickname(it)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // val doneButton: Button = findViewById(R.id.done_button)
+        // doneButton.setOnClickListener {
+        //    addNickname(it)
+        //  }
+        binding.myName = name
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
         }
 
     }
-    private  fun addNickname(view: View){
-        val nickname:EditText=findViewById(R.id.nickname_edit_text)
-        val nicknameTextView:TextView=findViewById(R.id.nickname_text_view)
-        nicknameTextView.text=nickname.text
-        nickname.visibility=View.GONE
-        nicknameTextView.visibility=View.VISIBLE
 
-        val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
+    private fun addNickname(view: View) {
+//        val nickname: EditText = findViewById(R.id.nickname_edit_text)
+//        val nicknameTextView: TextView = findViewById(R.id.nickname_text_view)
+        //binding.apply to make data easir to read
+        binding.apply {
+           // nicknameTextView.text = nicknameEditText.text
+            myName?.nickName=nicknameEditText.text.toString()
+            nicknameEditText.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+            invalidateAll()
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ AboutMe/app/src/main/java/com/deepak/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package com.deepak.aboutme
+
+data class MyName(var name:String="",var nickName:String="") {
+}
