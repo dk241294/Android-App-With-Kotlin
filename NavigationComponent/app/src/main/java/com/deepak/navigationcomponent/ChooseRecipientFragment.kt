a2f3a0be3a641eb4c6d60dd9881ca5d199f28f7e
deepak kumar <deepak24121994@gmail.com>
2019-12-18 13:36:17 +0530

    sending to and amount set

--- NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/ChooseRecipientFragment.kt
+++ NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/ChooseRecipientFragment.kt
@@ -2,19 +2,24 @@ package com.deepak.navigationcomponent
 
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipientFragment : Fragment(),View.OnClickListener {
-    var navController:NavController?=null
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+    var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +31,27 @@ class ChooseRecipientFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        navController=Navigation.findNavController(view)
+        navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
         view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
 
     }
 
     override fun onClick(p0: View?) {
-        when(p0!!.id){
-            R.id.next_btn->navController?.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
-            R.id.cancel_btn->activity!!.onBackPressed()
+        when (p0!!.id) {
+            R.id.next_btn ->
+                if (!TextUtils.isEmpty(input_recipient.text.toString())) {
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController?.navigate(
+                        R.id.action_chooseRecipientFragment_to_specifyAmountFragment,
+                        bundle
+                    )
+                } else {
+                    Toast.makeText(activity, "Enter a name", Toast.LENGTH_SHORT).show()
+                }
+
+
+            R.id.cancel_btn -> activity!!.onBackPressed()
         }
 
     }
--- NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/SpecifyAmountFragment.kt
+++ NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/SpecifyAmountFragment.kt
@@ -2,19 +2,29 @@ package com.deepak.navigationcomponent
 
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SpecifyAmountFragment : Fragment(),View.OnClickListener {
-    var navController:NavController?=null
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+    var navController: NavController? = null
+    lateinit var recipient: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = (arguments!!.getString("recipient")).toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +36,30 @@ class SpecifyAmountFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        navController=Navigation.findNavController(view)
+        navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
         view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        val message = "sending money to $recipient"
+        view.findViewById<TextView>(R.id.recipient).text = message
     }
+
     override fun onClick(p0: View?) {
-       when(p0!!.id){
-           R.id.send_btn->navController?.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
-           R.id.cancel_btn->activity!!.onBackPressed()
-       }
+        when (p0!!.id) {
+            R.id.send_btn ->
+                if (!TextUtils.isEmpty(input_amount.text.toString())) {
+                    val bundle = bundleOf("amount" to input_amount.text.toString())
+                    navController?.navigate(
+                        R.id.action_specifyAmountFragment_to_confirmationFragment,
+                        bundle
+                    )
+                } else {
+                    Toast.makeText(activity, "enter amount", Toast.LENGTH_SHORT).show()
+                }
+
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
     }
 
 
 }
+
