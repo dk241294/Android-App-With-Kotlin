d994aac2cb3922d5d879edc92814798dfcb6ae34
deepak kumar <deepak24121994@gmail.com>
2019-12-18 13:36:41 +0530

    confirming recipient and amount

--- NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/ConfirmationFragment.kt
+++ NavigationComponent/app/src/main/java/com/deepak/navigationcomponent/ConfirmationFragment.kt
@@ -6,11 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.os.bundleOf
 
 /**
  * A simple [Fragment] subclass.
  */
 class ConfirmationFragment : Fragment() {
+    lateinit var recipient: String
+    lateinit var amount: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = arguments!!.getString("recipient").toString()
+        amount = arguments!!.getString("amount").toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +28,12 @@ class ConfirmationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var confirmation="you have sent $ $amount to $recipient"
+        view.findViewById<TextView>(R.id.confirmation_message).text=confirmation
     }
 
 
