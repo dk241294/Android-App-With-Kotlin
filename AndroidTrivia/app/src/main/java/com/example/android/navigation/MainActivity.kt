d422992a0fde154f80522b072f1486ad16e564f1
deepak kumar <deepak24121994@gmail.com>
2019-12-26 16:59:21 +0530

    navigation drawer

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var drawLayout:DrawerLayout
+    private lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
+
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController=findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        drawLayout= binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this,navController,drawLayout)
+      //  appBarConfiguration = AppBarConfiguration(navController.graph,drawLayout)
+        NavigationUI.setupWithNavController(binding.navView,navController)
+
+
 
     }
-
-    override fun onNavigateUp(): Boolean {
-        val navController=findNavController(R.id.navHostFragment)
-        return navController.navigateUp()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController,drawLayout)
     }
+
 
 }
