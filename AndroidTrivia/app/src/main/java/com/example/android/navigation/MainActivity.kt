8a30abf6daea841df4c4c40364889828524aef61
deepak kumar <deepak24121994@gmail.com>
2019-12-26 22:34:11 +0530

    prevent nav gesture if not on start destination

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -5,10 +5,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 lateinit var drawLayout:DrawerLayout
@@ -21,6 +24,14 @@ lateinit var drawLayout:DrawerLayout
         val navController=findNavController(R.id.navHostFragment)
         drawLayout= binding.drawerLayout
         NavigationUI.setupActionBarWithNavController(this,navController,drawLayout)
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
       //  appBarConfiguration = AppBarConfiguration(navController.graph,drawLayout)
         NavigationUI.setupWithNavController(binding.navView,navController)
 
@@ -31,6 +42,8 @@ lateinit var drawLayout:DrawerLayout
         val navController = this.findNavController(R.id.navHostFragment)
         return NavigationUI.navigateUp(navController,drawLayout)
     }
+
+
 
 
 }
